3f40c24a7cfdf7d284c103e40bfe4e4c06273df2
allensam88 <54912919+allensam88@users.noreply.github.com>
2019-12-06 08:40:32 -0800

    got GET by id and POST for projects done

--- api/server.js
+++ api/server.js
@@ -5,7 +5,7 @@ server.use(express.json());
 const projectRouter = require('../routers/projectRouter.js');
 const actionRouter = require('../routers/actionRouter.js'); 
 server.use('/api/projects', projectRouter);
-server.use('/api/actions', projectRouter);
+server.use('/api/actions', actionRouter);
 
 server.get('/', (req, res) => {
     res.send(`<h2>Welcom to your projects & actions!</h2>`);
--- routers/projectRouter.js
+++ routers/projectRouter.js
@@ -5,61 +5,43 @@ const router = express.Router();
 const projectModel = require('../data/helpers/projectModel.js');
 const actionModel = require('../data/helpers/actionModel.js');
 
-// router.post('/', validateUser, (req, res) => {
-//     const userData = req.body;
-//     userDb.insert(userData)
-//         .then(newUser => {
-//             res.status(201).json(newUser);
+router.get('/:id', (req, res) => {
+    projectModel.get(req.params.id)
+        .then(project => {
+            res.status(200).json({ project })
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({ error: "The project could not be retrieved." });
+        });
+});
+
+router.post('/', (req, res) => {
+    const projectData = req.body;
+    projectModel.insert(projectData)
+        .then(newProject => {
+            res.status(201).json(newProject);
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({ error: "There was an error while saving the new project to the database." });
+        });
+});
+
+// router.put('/:id', validateUserId, (req, res) => {
+//     const changes = req.body;
+//     const id = req.params.id;
+//     userDb.update(id, changes)
+//         .then(count => {
+//             userDb.getById(id)
+//                 .then(updatedUser => {
+//                     res.status(200).json({ updatedUser, message: "The user has been modified." });
+//                 })
 //         })
 //         .catch(error => {
 //             console.log(error);
-//             res.status(500).json({ error: "There was an error while saving the new user to the database." });
+//             res.status(500).json({ error: "The user information could not be modified." });
 //         });
-// });
-
-// router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
-//     const postData = req.body;
-//     postDb.insert(postData)
-//         .then(newPost => {
-//             res.status(201).json(newPost);
-//         })
-//         .catch(error => {
-//             console.log(error);
-//             res.status(500).json({ error: "There was an error while saving the new post to the database." });
-//         });
-// });
-
-// router.get('/', (req, res) => {
-//     userDb.get(req)
-//         .then(users => {
-//             res.status(200).json({ users })
-//         })
-//         .catch(error => {
-//             console.log(error);
-//             res.status(500).json({ error: "The users could not be retrieved." });
-//         });
-// });
-
-// router.get('/:id', validateUserId, (req, res) => {
-//     userDb.getById(req.params.id)
-//         .then(user => {
-//             res.status(200).json({ user })
-//         })
-//         .catch(error => {
-//             console.log(error)
-//             res.status(500).json({ error: "The user could not be retrieved." })
-//         })
-// });
-
-// router.get('/:id/posts', (req, res) => {
-//     userDb.getUserPosts(req.params.id)
-//         .then(userPosts => {
-//             res.status(200).json({ userPosts })
-//         })
-//         .catch(error => {
-//             console.log(error)
-//             res.status(500).json({ error: "The user could not be retrieved." })
-//         })
 // });
 
 // router.delete('/:id', validateUserId, (req, res) => {
@@ -79,20 +61,6 @@ const actionModel = require('../data/helpers/actionModel.js');
 //         });
 // });
 
-// router.put('/:id', validateUserId, (req, res) => {
-//     const changes = req.body;
-//     const id = req.params.id;
-//     userDb.update(id, changes)
-//         .then(count => {
-//             userDb.getById(id)
-//                 .then(updatedUser => {
-//                     res.status(200).json({ updatedUser, message: "The user has been modified." });
-//                 })
-//         })
-//         .catch(error => {
-//             console.log(error);
-//             res.status(500).json({ error: "The user information could not be modified." });
-//         });
-// });
+
 
 module.exports = router;
